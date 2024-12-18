3edb0d74e3c11bed996a390fb6ed69b75a0c3202
meIrumKhan <meirum880@gmail.com>
2024-12-19 00:26:08 +0500

    update backend url

--- src/components/lib/handleFetch/FetchData.js
+++ src/components/lib/handleFetch/FetchData.js
@@ -26,7 +26,7 @@ export async function Fetchdata(method, url, body, form) {
 
    // const fullUrl = `http://localhost:8000${url}`;
   
-    const fullUrl = `https://fyp-reservation-backend.vercel.app${url}`;
+    const fullUrl = `https://fyp-backend-ivory.vercel.app${url}`;
   
   const options = {
     method: method,
