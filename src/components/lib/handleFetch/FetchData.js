8b91155fc5676a736d108892b9d7f1ffa2fcb0a0
meIrumKhan <meirum880@gmail.com>
2024-12-19 15:08:39 +0500

    update changes

--- src/components/lib/handleFetch/FetchData.js
+++ src/components/lib/handleFetch/FetchData.js
@@ -43,9 +43,9 @@ export async function Fetchdata(method, url, body, form) {
 
    // const fullUrl = `http://localhost:8000${url}`;
   
-    // const fullUrl = `https://fyp-backend-ivory.vercel.app${url}`;
+   const fullUrl = `https://fyp-backend-ivory.vercel.app${url}`;
     
-    const fullUrl = `https://flight-reservation-backend-registration.vercel.app${url}`;
+  
 
   
   const options = {
